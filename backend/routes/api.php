4d2914f07c7e0e7f660d2ca3d296059498a7440f
dalal <dalalarab6@gmail.com>
2026-01-09 14:28:48 +0100

    feat: système de messagerie interne - conversations + envoi de messages

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\PropertyController;
+use App\Http\Controllers\Api\MessageController;
 
 // Routes publiques (sans authentification)
 Route::prefix('auth')->group(function () {
@@ -19,4 +20,10 @@ Route::middleware('auth:api')->group(function () {
         Route::post('/refresh', [AuthController::class, 'refresh']);
     });
     Route::apiResource('properties', PropertyController::class);
+    Route::get('conversations', [MessageController::class, 'conversations']);
+    Route::get('messages/{userId}', [MessageController::class, 'messages']);
+    Route::post('messages/send', [MessageController::class, 'send']);
+    Route::get('messages-unread-count', [MessageController::class, 'unreadCount']);
+    Route::post('messages/{id}/mark-read', [MessageController::class, 'markAsRead']);
+    Route::delete('messages/{id}', [MessageController::class, 'destroy']);
 });
